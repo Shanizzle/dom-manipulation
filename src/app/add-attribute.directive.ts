b002ea7a9ac78a68474cd87dddc6b7f6b10e78fe
REESE <shannen.reese@howcogroup.com>
2020-05-05 16:32:32 -0500

    using renderer2

--- src/app/add-attribute.directive.ts
+++ src/app/add-attribute.directive.ts
@@ -1,7 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer2, OnInit } from '@angular/core';
 
 @Directive({
-  selector: '[addAttribute]'
+	selector: '[addAttribute]'
 })
-export class AddAttributeDirective {
+export class AddAttributeDirective implements OnInit {
+	@Input() addAttribute;
+
+	constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+
+	ngOnInit() {
+		// Accessing the native element directly:
+		// this.elementRef.nativeElement.setAttribute(this.addAttribute, '');
+
+		// Accessing using the Renderer2 class:
+		// this.renderer.setAttribute(elementRef, attribute, value);
+		this.renderer.setAttribute(this.elementRef.nativeElement, this.addAttribute, '');
+	}
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: `<span>I want to be red</span>`,
-  styles: ['[highlight] {color: red}']
+	selector: 'app-root',
+	template: `<span [addAttribute]="'highlight'">I want to be red</span>`,
+	styles: [ '[highlight] {color: red}' ]
 })
-export class AppComponent {
-}
-
+export class AppComponent {}
